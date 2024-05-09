9197dcf7029d977ab2eab5e107a7796fedf59a9f
rhit-carterxp <carterxp@rose-hulman.edu>
2024-05-08 20:22:38 -0400

    its working

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,67 +5,67 @@ rhit.main = function () {
 
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
-            const displayName = user.displayName;
-            const email = user.email;
-            const photoURL = user.photoURL;
-            const phoneNumber = user.phoneNumber;
-            const isAnonymous = user.isAnonymous;
-            const uid = user.uid;
-
-            console.log("The user is signed in ", uid);
-            console.log('displayName :>> ', displayName);
-            console.log('email :>> ', email);
-            console.log('photoURL :>> ', photoURL);
-            console.log('phoneNumber :>> ', phoneNumber);
-            console.log('isAnonymous :>> ', isAnonymous);
-            console.log('uid :>> ', uid);
+            console.log("The user is signed in ", user.uid);
         } else {
             console.log("There is no user signed in!");
         }
     });
 
-    const inputEmailEl = document.querySelector("#inputEmail");
-    const inputPasswordEl = document.querySelector("#inputPassword");
-
-    document.querySelector("#signOutButton").onclick = (event) => {
-        console.log(`Sign out`);
-        firebase.auth().signOut().then(function () {
-            console.log("You are now signed out");
-            window.location.href = 'index.html'
-        }).catch(function (error) {
-            console.log("Sign out error");
-        });
-    };
-
-    document.querySelector("#createAccountButton").onclick = (event) => {
-        console.log(`Create account for email: ${inputEmailEl.value} password:  ${inputPasswordEl.value}`);
-        firebase.auth().createUserWithEmailAndPassword(inputEmailEl.value, inputPasswordEl.value).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log("Create account error", errorCode, errorMessage);
-        });
-    };
-    document.querySelector("#logInButton").onclick = (event) => {
-        console.log(`Log in for email: ${inputEmailEl.value} password:  ${inputPasswordEl.value}`);
-        firebase.auth().signInWithEmailAndPassword(inputEmailEl.value, inputPasswordEl.value).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log("Existing account log in error", errorCode, errorMessage);
-        });
-    };
-
-    document.querySelector("#anonymousAuthButton").onclick = (event) => {
-        firebase.auth().signInAnonymously().catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log("Anonymous auth error", errorCode, errorMessage);
-        });
-    };
+    this.setUpAuthEventListeners();
     rhit.startFirebaseUI();
 };
 
+document.addEventListener('DOMContentLoaded', function() {
+    rhit.setUpEventHandlers();
+});
+
+rhit.setUpEventHandlers = function() {
+    document.querySelector("#submitNumber").addEventListener("click", rhit.generateBracket);
+};
+
+rhit.setUpAuthEventListeners = function() {
+    document.querySelector("#signOutButton").onclick = () => firebase.auth().signOut();
+    document.querySelector("#createAccountButton").onclick = () => {
+        const email = document.querySelector("#inputEmail").value;
+        const password = document.querySelector("#inputPassword").value;
+        firebase.auth().createUserWithEmailAndPassword(email, password);
+    };
+    document.querySelector("#logInButton").onclick = () => {
+        const email = document.querySelector("#inputEmail").value;
+        const password = document.querySelector("#inputPassword").value;
+        firebase.auth().signInWithEmailAndPassword(email, password);
+    };
+    document.querySelector("#anonymousAuthButton").onclick = () => {
+        firebase.auth().signInAnonymously();
+    };
+};
+
+rhit.generateBracket = function() {
+    const bracketSize = parseInt(document.querySelector("#numberInput").value);
+    if (isNaN(bracketSize) || bracketSize <= 0) {
+        alert("Please enter a valid number!");
+        return;
+    }
+
+    document.getElementById("myModal").style.display = "none";
+    const bracketContainer = document.getElementById("bracketContainer");
+    bracketContainer.innerHTML = "";
+
+    const bracket = document.createElement("div");
+    bracket.className = "bracket";
+    for (let i = 0; i < bracketSize; i++) {
+        const entrant = document.createElement("div");
+        entrant.className = "entrant";
+        entrant.textContent = `Entrant ${i + 1}`;
+        bracket.appendChild(entrant);
+    }
+    bracketContainer.appendChild(bracket);
+    bracketContainer.style.display = "block";
+};
+
 rhit.startFirebaseUI = function() {
-    var uiConfig = {
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-auth-container', {
         signInSuccessUrl: '/main.html',
         signInOptions: [
             firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -73,9 +73,7 @@ rhit.startFirebaseUI = function() {
             firebase.auth.PhoneAuthProvider.PROVIDER_ID,
             firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
         ],
-    };
-    const ui = new firebaseui.auth.AuthUI(firebase.auth());
-    ui.start('#firebaseui-auth-container', uiConfig);
-}
+    });
+};
 
 rhit.main();

7973ead650a29f1c16be0e746ec559705eb2df9e
rhit-carterxp <carterxp@rose-hulman.edu>
2024-05-10 19:21:38 -0400

    s

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -66,7 +66,7 @@ rhit.tournamentManager = {
         document.querySelector("#submitName").style.display = "block";
         document.querySelector("#entrantNameInput").focus();
 
-        const userRef = rhit.fbAuthManager.db.collection('users').doc(user.uid);
+        const userRef = rhit.fbAuthManager.db.collection('users').doc(rhit.fbAuthManager.auth.currentUser.uid);
         return userRef.set({
             entrants: [],
             totalEntrants: num,
